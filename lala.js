c0bf247fb6247557d44d71695a59a14257f9c0be
Ivan Lezcano <gabrielivanlezcano@gmail.com>
2023-11-14 02:36:47 -0300

    asdasd

---
+++ lala.js
@@ -0,0 +1,64 @@
+let Disfraz={
+    name:"spiderman",
+    fechaDeConfeccion:"12/12/2005",
+    nivelDeGracia:5,
+    puntajeDelDisfraz:10,
+
+}
+
+class Invitado {
+    constructor(edad,disfraz) {
+      this.edad = edad;
+      this.disfraz=disfraz
+    
+    }
+  }
+/*que exista un disfraz*/
+console.log(Disfraz)
+/*que funcione un atributo de la clase*/
+let juan = new Invitado(23,Disfraz)
+console.log(juan.disfraz)
+
+
+
+
+
+
+
+
+/*
+
+
+class invitado(disfraz=momia,personalidad=alegre){
+    disfraz=disfraz
+    personalidad=cambiante
+    edad=
+    estaciturnasexi?=self.edad<30
+    intermcambiartraje(self.disfraz=invitado.disfraz)
+    }
+    
+    class disfraz{
+    nombre="asdas"
+    fechadeconfeccion="11/12/1997"
+    niveldegracia=2
+    tobadas=
+    caretas
+    essexy?(persona.alegre)
+    estaconforme?=disfraz.puntaje>10
+    puntodeldisfraz()
+    
+    }
+    
+    class fiesta{
+    asistente=""
+    agregarAsistente(invitado)
+    lugar
+    fecha
+    invitados
+    fiestaesunbodrio{
+    self.invitados map.estaconforme?
+    }
+    fiestainolvidable map todos confirmes y sexuyes
+    }
+    
+    */
